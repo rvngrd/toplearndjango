7f5b3c464316381da79cfcff34e4eac5548e986c
rvngrd <rrvngrdd@gmail.com>
2023-02-26 21:05:13 +0330

    using dictionary and get method

--- ali/views.py
+++ ali/views.py
@@ -3,10 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+a = {
+    '1': 'salam',
+    '2': 'bye'
+}
+
 
 def index(request):
     return HttpResponse("salam")
 
 
 def dynamic(request, id):
-    return HttpResponse(id)
+    return HttpResponse(f'{id} and {a.get(id)}')
