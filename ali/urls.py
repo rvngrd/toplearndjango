35609a13fbeb1b4481ed52eeda9e5d5b9924f12a
rvngrd <rrvngrdd@gmail.com>
2023-03-01 14:03:09 +0330

    using {% url %} and sendig parameter

--- ali/urls.py
+++ ali/urls.py
@@ -6,6 +6,6 @@ app_name = 'ali'
 urlpatterns = [
     path('', views.link),
     path('sunday', views.index, name='iman'),
-    path('<int:id>', views.dynamic),
+    path('<int:id>', views.dynamic, name='dynamic'),
     path('monday', views.test_redirect)
 ]
